ce4949ffb33cb9d5dcefce9b73cbd313d9a3ae5c
mutiat <mutiat900@gmail.com>
2023-08-27 08:54:02 +0100

    updated

--- src/script.js
+++ src/script.js
@@ -2,18 +2,13 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: [
-            'Completed',
-            'Ongoing',
-            'Stalled'
-        ],
         datasets: [{
             label: 'My First Dataset',
-            data: [300, 50, 100],
+            data: [29, 71],
             backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
+            'rgb(0,139,139)',
+            'rgb(128, 97, 128)',
+            
             ],
             hoverOffset: 4
         }]
